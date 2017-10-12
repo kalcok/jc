5cdd1e306b389883f737dccdd8ba48fa1e5e516c
Martin Kalcok <martin.kalcok@gmail.com>
2017-10-12 23:17:20 +0200

    Query tests

--- tests/fixtures.go
+++ tests/fixtures.go
@@ -15,4 +15,12 @@ type (
 		MyID int      `bson:"_id"json:"myID"`
 		Data string   `bson:"data"json:"data"`
 	}
+	ExplicitCollection struct {
+		jc.Collection `bson:"-"json:"-"jc:"my_collection"`
+		Data string   `bson:"data"json:"data"`
+	}
+	ImplicitCollection struct {
+		jc.Collection `bson:"-"json:"-"`
+		Data string   `bson:"data"json:"data"`
+	}
 )
